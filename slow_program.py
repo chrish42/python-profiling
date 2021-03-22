33fa4db2db1f9336f7e3ac5b6191646074b7bfd9
Christian Hudon <chrish@pianocktail.org>
2021-03-22 17:05:12 -0400

    More examples of slowness

--- slow_program.py
+++ slow_program.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 """A slow program... or really, any program that hasn't been profiled yet."""
 
+import time
+
 
 def fib(n: int) -> int:
     """A recursive implementation of computation of Fibonacci numbers in Python. Will be slow in pretty much all languages, actually."""
@@ -18,8 +20,29 @@ def fib(n: int) -> int:
         return fib(n - 1) + fib(n - 2)
 
 
-def main():
+def function_that_recurses_too_much():
     print(fib(33))
+
+
+def function_that_busy_loops():
+    start = time.time()
+    while time.time() - start < 1:
+        pass
+
+
+def function_that_waits():
+    time.sleep(1)
+
+
+def function_that_allocates_a_lot():
+    arr = bytearray(b"\x01") * 10 ** 9
+
+
+def main():
+    function_that_busy_loops()
+    function_that_recurses_too_much()
+    function_that_allocates_a_lot()
+    function_that_waits()
 
 
 if __name__ == "__main__":
